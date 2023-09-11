038cc6c7df4ae44803c922b99d86b3c04a45e464
pbicskei <53083249+pbicskei@users.noreply.github.com>
2023-09-11 06:32:55 +0000

    Update version to v0.0.1

--- apps/entrypoint.sh
+++ apps/entrypoint.sh
@@ -11,7 +11,7 @@ fi
 # Controleer of de SSH-sleutelbestanden aanwezig zijn in de container.
 if [ ! -f "/home/ansible/.ssh/id_rsa" ] || [ ! -f "/home/ansible/.ssh/id_rsa.pub" ]; then
     # Als de sleutels ontbreken, toon dan een leuke tekstbanner met de boodschap dat we in een "testmodus" werken.
-    python3 -c "import pyfiglet; font = pyfiglet.figlet_format('Testrun', font='slant'); print(font); print('SSH keys are absent. Running on localhost powdered milk today!\n')"
+    python3 -c "import pyfiglet; font = pyfiglet.figlet_format('Testrun v0.0.1', font='slant'); print(font); print('SSH keys are absent. Running on localhost powdered milk today!\n')"
 else
     # Als de sleutels aanwezig zijn, zorg er dan voor dat ze de juiste toegangsrechten hebben.
     sudo chown -R ansible:ansible /home/ansible/.ssh  # Geef eigendom aan de "ansible" gebruiker.
