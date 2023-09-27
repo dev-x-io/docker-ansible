307afae610fc099bfd2a4943d84796336d0605b6
pbicskei <53083249+pbicskei@users.noreply.github.com>
2023-09-27 18:50:25 +0000

    Update script, allow multiple arguments

--- apps/entrypoint.py
+++ apps/entrypoint.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# Import necessary modules.
 import os
 import sys
 import subprocess
@@ -9,14 +8,11 @@ import json
 # Fetch the APP_VERSION environment variable. If not set, default to 'v0.0.0'.
 container_version = os.environ.get('APP_VERSION', 'v0.0.0')
 
-# This script is the entry point for our Docker container.
-# It initializes the environment, checks for requirements, and sets up SSH keys, among other tasks.
-
-# Check for the presence of an Ansible 'requirements.yml' file.
+# Check for Ansible requirements.yml
 if os.path.isfile("requirements.yml"):
     subprocess.run(["ansible-galaxy", "install", "-r", "requirements.yml", "-p", "/ansible/roles"])
 
-# Check if SSH key files are present in the container.
+# Check for SSH keys
 if not os.path.isfile("/home/ansible/.ssh/id_rsa") or not os.path.isfile("/home/ansible/.ssh/id_rsa.pub"):
     print(f'Testrun {container_version}')
     print('SSH keys are absent. Running on localhost powdered milk today!\n')
@@ -25,7 +21,7 @@ else:
     subprocess.run(["sudo", "chmod", "600", "/home/ansible/.ssh/*"])
     subprocess.run(["sudo", "chmod", "644", "/home/ansible/.ssh/*.pub"])
 
-# Check for '--deliver-shell' argument.
+# Check for --deliver-shell argument
 if '--deliver-shell' in sys.argv:
     with open('commands.json', 'r') as file:
         commands = json.load(file)
@@ -33,14 +29,13 @@ if '--deliver-shell' in sys.argv:
     print("Delivering shell")
     subprocess.run(["/bin/bash"])
 else:
-    # If a command is provided, execute it.
+    # Process the command and arguments
     if len(sys.argv) > 1:
-        args_list = sys.argv[1].split()  # Assume the command is the first argument and split it into a list.
-        print(f"Executing command: {args_list}")
+        full_command = sys.argv[1:]
         try:
-            subprocess.run(args_list)
+            subprocess.run(full_command)
         except FileNotFoundError as e:
             print(f"An error occurred: {e}")
-            print(f"Could not find command: {args_list[0]}")
+            print(f"Could not find command: {full_command[0]}")
     else:
         print("No command provided. Container has ended without applying your secret sauce to success.")
